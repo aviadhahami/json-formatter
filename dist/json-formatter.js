48028cd24332e6cbf524626f03ea0cd11da71133
Aviad Hahami <aviadhahami@gmail.com>
2015-04-29 14:01:05 +0300

    Revert "started modified dist"
    This reverts commit 5ae906b5ae25b768f1fbe6de922b08b4126e1684.

--- dist/json-formatter.js
+++ dist/json-formatter.js
@@ -57,11 +57,6 @@ angular.module('jsonFormatter', ['RecursionHelper'])
       }
     };
     scope.type = getType(scope.json);
-      
-    ///MODIFICATIONS BASED ON CLASS
-      
-      
-    //END MODIFICATIONS
     scope.hasKey = typeof scope.key !== 'undefined';
     scope.getConstructorName = function(){
       return getObjectName(scope.json);
