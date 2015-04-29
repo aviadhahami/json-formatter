70d34f5a7fb5a648aa11626e78127fd0d6a436c6
Aviad Hahami <aviadhahami@gmail.com>
2015-04-29 14:55:55 +0300

    Added color toggling

--- demo/demo.js
+++ demo/demo.js
@@ -2,15 +2,20 @@ var app = angular.module('demo', ['ngSanitize', 'jsonFormatter']);
 
 app.controller('MainCtrl', function($scope) {
     //coloring setup
-    $scope.viewType = "result dark";
-    $scope.viewClass = "json-formatter-dark";
-    
+    // $scope.viewType = 'result dark';
+    //$scope.viewClass = 'json-formatter-dark';
+
+    $scope.colorCheck = false;
+
+
+
+
     //textarea place holder
     $scope.taPlaceHolder = 'Place your JSON here';
-    
+
     //message stub
     var BAD_JSON = 'Please Make sure your JSON string is valid!';
-    
+
     //Watch function for textarea changes
     $scope.$watch('textarea', function(str) {
         var result = {};
@@ -21,5 +26,12 @@ app.controller('MainCtrl', function($scope) {
             // console.log(e);
             $scope.complex = BAD_JSON;
         }
+    });
+
+
+    $scope.$watch('colorCheck', function(bool) {
+        //console.log(bool);
+        $scope.viewType = $scope.colorCheck ? 'result dark' : 'result';
+        $scope.viewClass = $scope.colorCheck ? 'json-formatter-dark' : '';
     });
 });
