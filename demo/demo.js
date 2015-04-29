6065e46ef63072cb548a831d4743886a1bc96c64
Aviad Hahami <aviadhahami@gmail.com>
2015-04-29 13:45:49 +0300

    more changes

--- demo/demo.js
+++ demo/demo.js
@@ -1,17 +1,25 @@
 var app = angular.module('demo', ['ngSanitize', 'jsonFormatter']);
 
 app.controller('MainCtrl', function($scope) {
+    //coloring setup
     $scope.viewType = "result dark";
     $scope.viewClass = "json-formatter-dark";
-
-
-
+    
+    //textarea place holder
+    $scope.taPlaceHolder = 'Place your JSON here';
+    
+    //message stub
+    var BAD_JSON = 'Please Make sure your JSON string is valid!';
+    
+    //Watch function for textarea changes
     $scope.$watch('textarea', function(str) {
         var result = {};
         try {
             $scope.complex = JSON.parse(str);
+            //console.log(!!JSON.parse(str));
         } catch (e) {
-            console.log(e);
+            // console.log(e);
+            $scope.complex = BAD_JSON;
         }
     });
 });
